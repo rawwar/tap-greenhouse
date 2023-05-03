456abc3d6dd142a3fb7b72502179dca2f5036516
Kalyan <kalyan.ben10@live.com>
2023-05-03 18:10:21 +0000

    update candidates schema

--- tap_greenhouse/schemas/candidates.py
+++ tap_greenhouse/schemas/candidates.py
@@ -6,6 +6,80 @@ schema = th.PropertiesList(
     th.Property("first_name", th.StringType),
     th.Property("last_name", th.StringType),
     th.Property("company", th.StringType),
+    th.Property("title", th.StringType),
+    th.Property("is_private", th.BooleanType),
+    th.Property('application_ids', th.ArrayType(th.IntegerType)),
+    th.Property(
+        'phone_numbers',
+         th.ArrayType(
+            th.ObjectType(
+                th.Property("value", th.StringType),
+                th.Property("type", th.StringType)
+                )
+            )
+        ),
+    th.Property(
+        'addresses',
+        th.ArrayType(
+            th.ObjectType(
+                th.Property('value', th.StringType),
+                th.Property('type', th.StringType)
+            )
+        )
+    ),
+    th.Property(
+        'email_addresses',
+        th.ArrayType(
+            th.ObjectType(
+                th.Property('value', th.StringType),
+                th.Property('type', th.StringType)
+            )
+        )
+    ),
+    th.Property(
+        'website_addresses',
+        th.ArrayType(
+            th.ObjectType(
+                th.Property('value', th.StringType),
+                th.Property('type', th.StringType)
+            )
+        )
+    ),
+    th.Property(
+        'social_media_addresses',
+        th.ArrayType(
+            th.ObjectType(
+                th.Property('value', th.StringType)
+            )
+        )
+    ),
+    th.Property(
+        'recruiter',
+        th.ObjectType(
+            th.Property('id', th.IntegerType),
+            th.Property('first_name', th.StringType),
+            th.Property('last_name', th.StringType),
+            th.Property('name', th.StringType),
+            th.Property('employee_id', th.IntegerType),
+        )
+    ),
+    th.Property(
+        'coordinator',
+        th.ObjectType(
+            th.Property('id', th.IntegerType),
+            th.Property('first_name', th.StringType),
+            th.Property('last_name', th.StringType),
+            th.Property('name', th.StringType),
+            th.Property('employee_id', th.IntegerType),
+        )
+    ),
+    th.Property('can_email', th.BooleanType),
+    th.Property(
+        'tags',
+        th.ArrayType(th.StringType)
+    ),
+    th.Property('created_at', th.DateTimeType),
+    th.Property('updated_at', th.DateTimeType),
     th.Property("last_activity", th.DateTimeType),
-    th.Property("updated_at", th.DateTimeType),
+
 ).to_dict()
