1a721c666a51e3de2e062f869daf01056fbc1f1e
kalyanr <kalyan.ben10@live.com>
2023-05-06 03:42:04 +0530

    fix list demographic questions schema

--- tap_greenhouse/schemas/list_demographic_questions.py
+++ tap_greenhouse/schemas/list_demographic_questions.py
@@ -1,13 +1,24 @@
 """JSON Schema for Greenhouse API: List Demographic Questions."""
 from singer_sdk import typing as th
 
-schema = th.ArrayType(
-    th.ObjectType(
+schema = th.PropertiesList(
+    th.Properties(
         th.Property("id", th.IntegerType),
-        th.Property("title", th.StringType),
-        th.Property("description", th.StringType),
         th.Property("active", th.BooleanType),
+        th.Property("demographic_question_set_id", th.IntegerType),
+        th.Property("name", th.StringType),
+        th.Property(
+            "translations",
+            th.ArrayType(
+                th.Properties(
+                    th.Property("language", th.StringType),
+                    th.Property("name", th.StringType),
+                ),
+            ),
+        ),
+        th.Property("required", th.BooleanType),
     ),
-)
+).to_dict()
+
 
 schema_dict = schema.to_dict()
